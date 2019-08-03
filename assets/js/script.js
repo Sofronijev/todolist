3d9f8623678a120fa06e08ba2e2a4507eacf6bbb
Sofronijev <sofronijev@gmail.com>
2019-08-03 20:41:08 +0200

    js edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,6 +59,25 @@ function newTask() {
 
     //add everyting in li elemnt to table list
     document.getElementById("listTable").appendChild(listItem);
+    
+    
+    /*
+    // OVO NE RADI! Za delete i hoce da radi ali ponavlja se vise puta kad ima vise elemenata, addeventlistener dodaje event vise puta na jedno dugme, tako da ako ga doda paran broj puta on ce da toggle paran broj i izgledace kao da se nista ne desava, nisam nasao resenje osim da izbacim u global funkcije
+
+    //delete button on click
+    let deleteButton = document.querySelectorAll(".delete");    
+    for(let i = 0; i<deleteButton.length; i++){
+        deleteButton[i].addEventListener("click", function(){
+            this.parentElement.remove();
+        });       
+    }
+    let importantButton = document.querySelectorAll(".important");
+    for (let y = 0; y<importantButton.length; y++){
+         importantButton[y].addEventListener("click", function(){
+            this.classList.toggle("gold");            
+        });
+    }
+    */
 
     //clear input field
     addTask.value = "";
@@ -76,15 +95,10 @@ list.addEventListener('click', function (ev) {
         ev.target.classList.toggle("done");
         ev.target.parentElement.classList.toggle("lineThrough");
         if (ev.target.textContent === "check_box_outline_blank") {
-            ev.target.textContent = "check_box";
-            console.log(ev.target.parentElement);
-            console.log(doneList);
+            ev.target.textContent = "check_box";         
             doneList.appendChild(ev.target.parentElement);
-        } else {
-            ev.target.textContent = "check_box_outline_blank";
-        }
-    }
-    // delete li element on click
+        }  
+    }  
     if (ev.target.classList.contains("delete")) {
         ev.target.parentElement.remove();
     }
@@ -92,4 +106,22 @@ list.addEventListener('click', function (ev) {
     if (ev.target.classList.contains("important")) {
         ev.target.classList.toggle("gold");
     }
+});
+
+doneList.addEventListener('click', function (ev) {    
+    if (ev.target.classList.contains("checkbox")) {
+        ev.target.classList.toggle("done");
+        ev.target.parentElement.classList.toggle("lineThrough");
+        if (ev.target.textContent === "check_box") {
+            ev.target.textContent = "check_box_outline_blank";            
+            list.appendChild(ev.target.parentElement);
+        } 
+    } 
+    if (ev.target.classList.contains("delete")) {
+        ev.target.parentElement.remove();
+    }
+    //mark important
+    if (ev.target.classList.contains("important")) {
+        ev.target.classList.toggle("gold");
+    } 
 });
